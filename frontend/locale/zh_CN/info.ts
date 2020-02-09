1350debb7f7f523d0ec34764ad77d1409ef765d8
konata9 <maochichen@163.com>
2020-02-09 10:44:21 +0800

    feat:add login page layout
    login logic is not complete

--- frontend/locale/zh_CN/info.ts
+++ frontend/locale/zh_CN/info.ts
@@ -2,5 +2,9 @@ export default {
   placeholder: {
     username: '请输入用户名',
     password: '请输入密码'
+  },
+  validate: {
+    usernameIsNull: '用户名不能为空',
+    passwordIsNull: '密码不能为空'
   }
 }
--- frontend/src/modules/login/index.tsx
+++ frontend/src/modules/login/index.tsx
@@ -1,37 +1,66 @@
 import * as React from 'react'
-import { Card, Form, Input, Button } from 'antd'
+import { Card, Form, Input, Button, Icon } from 'antd'
+import { FormComponentProps } from 'antd/lib/form'
 
 import styled from 'styled-components'
 import { StyledLayout } from '../style/layout'
+import { fontDark } from '../style/style'
 
 import { formatMessage } from '../../utils'
 
 const CardWrapper = styled.div`
-  width: 55%;
+  width: 600px;
   margin: 0 auto;
+  margin-top: 15vh;
 `
 
 const CardTitle = styled.div`
-  
+  color: ${fontDark};
+  font-size: 2rem;
+  text-align: center;
 `
 
-const Login = Form.create()(() => {
-  const login = () => {
+type LoginForm = FormComponentProps
+const Login = Form.create()((props: LoginForm) => {
 
+  const { form: { getFieldDecorator, validateFields } } = props
+
+  const login = () => {
+    validateFields((error, values) => {
+      if (!error) {
+        console.log(values)
+      }
+    })
   }
 
   return (
     <StyledLayout>
       <CardWrapper>
         <Card title={
-          <CardTitle>欢迎登陆</CardTitle>
+          <CardTitle>{formatMessage('system.logo')}</CardTitle>
         }>
-          <Form>
+          <Form hideRequiredMark>
             <Form.Item>
-              <Input size="large" />
+              {
+                getFieldDecorator('username', {
+                  rules: [
+                    { required: true, message: formatMessage('validate.usernameIsNull') }
+                  ]
+                })(
+                  <Input size="large" placeholder={formatMessage('placeholder.username')} prefix={<Icon type="user" />} />
+                )
+              }
             </Form.Item>
             <Form.Item>
-              <Input size="large" />
+              {
+                getFieldDecorator('password', {
+                  rules: [
+                    { required: true, message: formatMessage('validate.passwordIsNull') }
+                  ]
+                })(
+                  <Input size="large" placeholder={formatMessage('placeholder.password')} prefix={<Icon type="lock" />} />
+                )
+              }
             </Form.Item>
             <Form.Item>
               <Button type="primary" size="large" block onClick={login}>{formatMessage('system.login')}</Button>
