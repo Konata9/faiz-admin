5a742a11ce306fe981ed36782e5893d2b9821e20
konata9 <maochichen@163.com>
2020-02-12 16:01:33 +0800

    feat:add menu create logic

--- frontend/locale/zh_CN/index.ts
+++ frontend/locale/zh_CN/index.ts
@@ -1,9 +1,11 @@
 import common from './common'
 import info from './info'
 import account from './account'
+import menu from './menu'
 
 export default {
   ...common,
   ...info,
-  ...account
+  ...account,
+  ...menu
 }
--- frontend/src/modules/layout/menu.tsx
+++ frontend/src/modules/layout/menu.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
 import { Menu } from 'antd'
+import { RouterLink } from '@modules/style/layout'
 import { MenuWrapper } from './style'
 import Logo from './logo'
 
 import { IStore, IRootStore } from '@store'
+
+const menuList = [
+  {
+    name: 'dashboard',
+    link: '/'
+  },
+  {
+    name: 'system',
+    submenu: [
+      {
+        name: 'user',
+        link: '/user'
+      },
+      {
+        name: 'role',
+        link: '/role'
+      }
+    ]
+  }
+]
 
 interface IProps {
   rootStore?: IRootStore
@@ -17,13 +38,30 @@ const MenuList = inject((stores: IStore) => {
 })(
   observer(
     (props: IProps) => {
+      const createMenuList = (menuList: Array<any>): any => {
+        return menuList.map(menu => {
+          const { submenu = [], name, link = '' } = menu
+          if (submenu.length > 0) {
+            return (
+              <Menu.SubMenu title={name} >
+                {...createMenuList(submenu)}
+              </Menu.SubMenu>
+            )
+          } else {
+            return (
+              <Menu.Item>
+                <RouterLink to={link}>{name}</RouterLink>
+              </Menu.Item>
+            )
+          }
+        })
+      }
+
       return (
         <MenuWrapper>
           <Logo />
-          <Menu theme="dark">
-            <Menu.SubMenu>
-
-            </Menu.SubMenu>
+          <Menu theme="dark" mode="inline">
+            {...createMenuList(menuList)}
           </Menu>
         </MenuWrapper>
       )
